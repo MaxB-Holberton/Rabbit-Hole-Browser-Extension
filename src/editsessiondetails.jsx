cd78087c0ba8375572c7295ff3f28185e5cd4b1f
MaxB-Holberton <12107@holbertonstudents.com>
2026-06-03 15:53:14 +1000

    additions to Session page

--- src/editsessiondetails.jsx
+++ src/editsessiondetails.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { RHDeletePage } from "./history.js";
+import { useEffect, useState } from 'react';
+import { RHDeletePage, RHEditPage } from "./history.js";
+import { ShowSessionTags } from "./viewsessiondetails";
 
 export function EditSessionPageList(data) {
 	//TODO: Edit btn to appear when edit page is
 	const pages = Array.isArray(data?.data) ? data.data : [];
+	const [show, setShow] = useState(false);
 	return (
 		<div>
-			<b>Pages: </b><button id="add_page">Add Page</button>
+			<b>Pages: </b>
+			<br/>
+			<button id="add_page">Add Page</button>
+			<button id="toggle_tags" onClick={() => {/* This will toggle displaying the tags */}}>Toggle Tags</button>
 			<ul>
 			{pages.map((item, index2) => (
 				<li id={index2} key={index2}>
-					<button id={`${index2}_edit`} onClick={() =>{} }>Edit</button>
- 					<button onClick={async () => { RHDeletePage(index2, data.session_key) }}>Delete</button>
+					<button id={`${index2}_edit`} onClick={() => { RHEditPage(index2, data.session_key, item.url); }}>Edit</button>
+ 					<button id={`${index2}_delete`} onClick={async () => { RHDeletePage(index2, data.session_key) }}>Delete</button>
 					<a id={`${index2}_a`} href={item.url}><span id={`${index2}_span`}>{item.title}</span></a>
+					<br/>
+					<div id={`${index2}_tagdiv`}>
+						<p>Category: <span>{item.category}</span></p>
+						<p>structTag: <span>{item.structuralTags}</span></p>
+						<p>ManualTags: <span>{item.manualTags}</span></p>
+					</div>
 				</li>
 			))}
 			</ul>
