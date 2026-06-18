37dbfc79563ed407b19abc4ae41c9ee9883c5b42
MaxB-Holberton <12107@holbertonstudents.com>
2026-06-18 16:31:04 +1000

    Creating Setting Page

--- src/settingspage.jsx
+++ src/settingspage.jsx
@@ -1,23 +1,45 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { RHGetPage } from "./history.js";
+import { Link } from 'react-router-dom';
 import { SectionRibbon } from "./viewsessiondetails";
 
 function SettingsOptionsList() {
 	return (
 		<>
-		<div className="rabbitHole">
-			<ul>
-				<li>Edit Blacklist</li>
-			</ul>
-		</div>
+			<div className="rabbitHole">
+				<ul>
+					<li><Link to="/settings/blacklist">Edit Blacklist</Link></li>
+				</ul>
+			</div>
 		</>
 	)
 }
 
 export function BlacklistEditPage() {
+	const [current_blacklist, ActiveBlacklist] = useState([]);
+	const [unselected_items, InActiveBlacklist] = useState([]);
+	const [all_blacklist_items, AllBlacklistItems] = useState([]);
+
+	useEffect(() => {
+		RHGetPage("Rabbithole_active_blacklist").then(data => ActiveBlacklist(data));
+		RHGetPage("Rabbithole_all_blacklist_items").then(data => AllBlacklistItems(data));
+	});
+
 	/*
-	 * TODO: Get blacklist edit array from local storage
-	 *
+	 * TODO: 2 lists (Inactive) | (Active)
+	 * Make both lists scrollable
+	 * Have btn to add/remove from active
 	 */
+
+	return (
+		<>
+			<div className="rabbitHole">
+				<p>{current_blacklist}</p>
+				<p>{all_blacklist_items}</p>
+			</div>
+		</>
+	);
 }
 
 export function SettingsPage() {
@@ -29,7 +51,7 @@ export function SettingsPage() {
 		{SectionRibbon('Settings')}
 		</div>
 		<section className="rabbitHole" id="settings">
-			{SettingsOptionsList()}
+		{SettingsOptionsList()}
 		</section>
 		<div id="previousPageContent">
 		<section id="previousBannerSection">
